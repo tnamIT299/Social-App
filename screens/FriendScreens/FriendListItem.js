e076d91a9018d7fe7c89b1ebde0598030a8d02fd
MSI PC <hao7ehoathanh@gmail.com>
2024-09-25 09:23:41 +0700

    Remove status friend

--- screens/FriendScreens/FriendListItem.js
+++ screens/FriendScreens/FriendListItem.js
@@ -1,13 +1,32 @@
 import React from "react";
-import { View, Text, StyleSheet, Image } from "react-native";
+import { View, Text, StyleSheet, Image, Button, Alert } from "react-native";
+import { handleRemoveFriend } from "./FriendFunction";
 
-const FriendListItem = ({ avatar, name }) => {
+const FriendListItem = ({ avatar, name, friendId, fetchFriendList }) => {
+  // Hàm xử lý xóa quan hệ bạn bè
+  const removeFriend = async () => {
+    const result = await handleRemoveFriend(friendId);
+
+    if (result.success) {
+      Alert.alert("Thành công", "Đã xóa quan hệ bạn bè thành công");
+      // Cập nhật lại danh sách bạn bè sau khi xóa thành công
+      fetchFriendList();
+    } else {
+      console.error(result.error);
+      Alert.alert(
+        "Lỗi",
+        result.error || "Đã xảy ra lỗi khi xóa quan hệ bạn bè"
+      );
+    }
+  };
+
   return (
     <View style={styles.requestContainer}>
       <Image source={{ uri: avatar }} style={styles.avatar} />
       <View style={styles.requestInfo}>
         <Text style={styles.name}>{name}</Text>
       </View>
+      <Button title="Xóa" color="#FF0000" onPress={removeFriend} />
     </View>
   );
 };
